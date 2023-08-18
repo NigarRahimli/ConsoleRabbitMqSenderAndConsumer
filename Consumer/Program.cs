f5d4c36898a0b793bc0122b3d424586b5e13364f
NigarRahimli <nigarrahimli07@gmail.com>
2023-08-18 15:59:48 +0400

    ready

---
+++ Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+//consumer
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "hello",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+    //
+    // Declare the exchange (same exchange name and type as used in the sender)
+    channel.ExchangeDeclare(exchange: "my_exchange",
+                            type: ExchangeType.Direct);
+    //
+    //
+
+    // Bind the queue to the exchange with the same routing key used in the producer
+    channel.QueueBind(queue: "hello",
+                      exchange: "my_exchange",
+                      routingKey: "my_routing_key");
+    //
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("Received: {0}", message);
+    };
+    channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+    Console.WriteLine("hello world.");
+    Console.ReadLine();
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+//sender
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://localhost:5672");
+using var connection=factory.CreateConnection();
+var channel=connection.CreateModel();
+//
+// adli exchange
+channel.ExchangeDeclare(exchange: "my_exchange",
+                        type: ExchangeType.Direct); // direct,fanout,topic
+//
+
+channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+//adli exchange
+// Bind the queue to the exchange with a specific routing key
+channel.QueueBind(queue: "hello",
+                  exchange: "my_exchange",
+                  routingKey: "my_routing_key"); // istenilen ad ola biler her irooting key ucun
+
+//
+Console.Write("Gondermek istediyiniz mesaj :");
+var message =Console.ReadLine();
+var body=Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(/*exchange: ""*/ //adsiz olanda quebind ve exchange declare ehtiyac qalmir
+                            exchange:"my_exchange",
+                              routingKey: "my_routing_key",
+                              basicProperties: null,
+                              body: body);
+Console.WriteLine("Message sent");
+Console.ReadLine();
